ce9de5132d3f766be72d77773a2a2b1e6efc0570
bensboto <bensboto@gmail.com>
2023-05-18 21:56:29 -0600

    fixed auth middleware

--- server/server.js
+++ server/server.js
@@ -23,11 +23,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+});
+
 // app.use(routes);
 
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
